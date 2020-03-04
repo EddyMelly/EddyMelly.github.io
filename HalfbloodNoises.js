f387093c73f0c342ca465e3853ccde8395a7c160
EddyMelly <57482261+EddyMelly@users.noreply.github.com>
2020-03-04 16:34:51 +0000

    elegant functions

--- HalfbloodNoises.js
+++ HalfbloodNoises.js
@@ -21,12 +21,7 @@ document.getElementById("status").style.color ="red";
 
 function ConnectTwitchChat() {
   const channel = "halfbloood";
-
-  
-
-
     // Connect to Twitch 
-    
     chat.connect().then(() => {
       chat.join(channel).then(() => {
         isConnected = true;
@@ -48,179 +43,133 @@ function ConnectTwitchChat() {
     });
 
     chat.on('*', message => {
+    var clean_message = DOMPurify.sanitize(message.message, { ALLOWED_TAGS: ['b'] });
 
-     // console.log(message.tags["customRewardId"]);
-   var clean_message = DOMPurify.sanitize(message.message, { ALLOWED_TAGS: ['b'] });
-
-  if((message.tags["customRewardId"] ==="99c5af66-501a-43cf-b71e-903dadbc0f31") ){
+    if((message.tags["customRewardId"] ==="99c5af66-501a-43cf-b71e-903dadbc0f31") ){
     console.log("Scav sound");
     console.log(clean_message);
     ScavSounds(clean_message);
-    
-  }else if(message.tags["customRewardId"] ==="feff768e-40e6-4888-9fcb-5ee7ad446e87"){
-   console.log("Grenade sound");
+    }
+    else if(message.tags["customRewardId"] ==="feff768e-40e6-4888-9fcb-5ee7ad446e87"){
+    console.log("Grenade sound");
     GrenadeSoundNew(clean_message);
-  }
-  else if(message.tags["customRewardId"] ==="0c0e8980-291f-4145-8155-5ea0d2a80f9b" ){
+    }
+    else if(message.tags["customRewardId"] ==="0c0e8980-291f-4145-8155-5ea0d2a80f9b" ){
     console.log("Raider sound");
     RaiderSounds(clean_message);
-  }
-  else{
-  }
-
+    }
+    else{
+    }
   })
-  };
+};
+
+function GatherElements(passedIntStart, passedIntEnd, passedName){
+  var elementList = new Array;
+  for(var i = passedIntStart; i < passedIntEnd+1; i++){
+    var tempElement = document.getElementById(passedName+i);
+    //console.log(tempElement);
+    elementList.push(tempElement);
+  }
+return elementList;
+};
 
 
 
 function ScavSounds(passedMessage){
-  console.log("working2");
-  console.log("passed message:" + passedMessage);
- 
-//aheadleft
-var sound5 = document.getElementById("scav5");
-var sound6 = document.getElementById("scav6");
-var sound8 = document.getElementById("scav8");
-var sound11 = document.getElementById("scav11");
-var sound16 = document.getElementById("scav16");
-var sound18 = document.getElementById("scav18");
-var sound20 = document.getElementById("scav20");
 
-//aheadright
-var sound1 = document.getElementById("scav1");
-var sound13 = document.getElementById("scav13");
-var sound15 = document.getElementById("scav15");
-var sound21 = document.getElementById("scav21");
-
-//left
-var sound2 = document.getElementById("scav2");
-var sound9 = document.getElementById("scav9");
-var sound14 = document.getElementById("scav14");
-var sound17 = document.getElementById("scav17");
-
-//right
-var sound3 = document.getElementById("scav3");
-var sound4 = document.getElementById("scav4");
-var sound7 = document.getElementById("scav7");
-var sound10 = document.getElementById("scav10");
-var sound12 = document.getElementById("scav12");
-var sound19 = document.getElementById("scav19");
+var scavList = new Array;
+scavList = GatherElements(1,21,"scav");
 
 
 if(passedMessage =="left"){
-  var sounds = new Array(sound2, sound9, sound14, sound17);
+  var sounds = new Array(scavList[1], scavList[8], scavList[13], scavList[16]);
   var randomSound = Math.floor(Math.random() * 4);
   PlaySound(sounds[randomSound]);
 }
-else if(passedMessage =="aheadleft"){
-  var sounds = new Array(sound5, sound6, sound8, sound11, sound16, sound18, sound20);
+else if(passedMessage =="leftahead"){
+  var sounds = new Array(scavList[4], scavList[5], scavList[7], scavList[10], scavList[15], scavList[17], scavList[19]);
    var randomSound = Math.floor(Math.random() * 7);
   PlaySound(sounds[randomSound]);
-
 }
-else if(passedMessage =="aheadright"){
-  var sounds = new Array(sound1, sound13, sound15, sound21);
+else if(passedMessage =="rightahead"){
+  var sounds = new Array(scavList[0], scavList[12], scavList[14], scavList[20]);
   var randomSound = Math.floor(Math.random() * 4);
  PlaySound(sounds[randomSound]);
-
 }
 else if(passedMessage == "right"){
-  var sounds = new Array(sound3, sound4, sound7, sound10, sound12, sound19);
-  var randomSound = Math.floor(Math.random() * 6);
- 
+  var sounds = new Array(scavList[2], scavList[3], scavList[6], scavList[9], scavList[11], scavList[18]);
+  var randomSound = Math.floor(Math.random() * 6); 
   PlaySound(sounds[randomSound]);
- 
-}else{
-var sounds = new Array(sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9, sound10, sound11, sound12, sound13, sound14, sound15, sound16, sound17, sound18, sound19, sound20, sound21);
-var randomSound = Math.floor(Math.random() * 21);
-PlaySound(sounds[randomSound]);
 }
-
+else{
+var randomSound = Math.floor(Math.random() * 21);
+PlaySound(scavList[randomSound]);
+}
 };
 
 function RaiderSounds(passedMessage){
-  console.log("raider working");
-//left
- var sound1 = document.getElementById("raider1");
-var sound5 = document.getElementById("raider5");
-//right
-var sound2 = document.getElementById("raider2");
-var sound4 = document.getElementById("raider4");
-//aheadleft
-var sound3 = document.getElementById("raider3");
-//aheadright
-var sound6 = document.getElementById("raider6");
+var raiderList = new Array;
+raiderList = GatherElements(1,6,"raider");
 
 if(passedMessage =="left"){
-  var sounds = new Array(sound1, sound5);
+  var sounds = new Array(raiderList[0], raiderList[4]);
   var randomSound = Math.floor(Math.random() * 2);
   PlaySound(sounds[randomSound]);
 }
 else if(passedMessage =="right"){
-  var sounds = new Array(sound2, sound4);
-   var randomSound = Math.floor(Math.random() * 2);
+  var sounds = new Array(raiderList[1], raiderList[3]);
+  var randomSound = Math.floor(Math.random() * 2);
   PlaySound(sounds[randomSound]);
 }
-else if(passedMessage =="aheadleft"){ 
- PlaySound(sound3);
+else if(passedMessage =="leftahead"){ 
+  PlaySound(raiderList[2]);
 }
-else if(passedMessage == "aheadright"){
-  PlaySound(sound6);
+else if(passedMessage == "rightahead"){
+  PlaySound(raiderList[5]);
 }else{
-var sounds = new Array(sound1, sound2, sound3, sound4, sound5, sound6);
-var randomSound = Math.floor(Math.random() * 6);
-PlaySound(sounds[randomSound]);
-  }
+  
+  var randomSound = Math.floor(Math.random() * 6);
+  PlaySound(raiderList[randomSound]);
+}
 };
 
 
 
 function GrenadeSounds(passedMessage){
-var sound1 = document.getElementById("grenade1");
-var sound2 = document.getElementById("grenade2");
-var sound3 = document.getElementById("grenade3");
-var sounds = new Array(sound1, sound2, sound3);
-var randomSound = Math.floor(Math.random() * 3);
-PlaySound(sounds[randomSound]);
+var grenadeList = new Array; 
+grenadeList = GatherElements(1,3,"grenade");
+
+var sounds = new Array(grenadeList[0], grenadeList[2]);
 };
+
+
+
+
 
 function GrenadeSoundNew(passedMessage){
 
-//left  
-var sound1 = document.getElementById("grenade4");
-//leftahead
-var sound4 = document.getElementById("grenade7");
-var sound6 = document.getElementById("grenade9");
-var sound9 = document.getElementById("grenade12");
-//right
-var sound2 = document.getElementById("grenade5");
-var sound5 = document.getElementById("grenade8");
-var sound7 = document.getElementById("grenade10");
-var sound8 = document.getElementById("grenade11");
-//rightahead
-var sound3 = document.getElementById("grenade6");
+var grenadeList = new Array;
+grenadeList = GatherElements(4,12,"grenade");
 
 if(passedMessage =="left"){
-  PlaySound(sound1);
+  PlaySound(grenadeList[0]);
 }
 else if(passedMessage =="rightahead"){
-  PlaySound(sound3);
+  PlaySound(grenadeList[2]);
 }
 else if(passedMessage =="leftahead"){ 
- var sounds = new Array(sound4, sound6, sound9);
-var randomSound = Math.floor(Math.random() * 3);
-PlaySound(sounds[randomSound]);
+  var sounds = new Array(grenadeList[3], grenadeList[5], grenadeList[8]);
+  var randomSound = Math.floor(Math.random() * 3);
+  PlaySound(sounds[randomSound]);
 }
 else if(passedMessage == "right"){
- var sounds = new Array(sound2, sound5, sound7, sound8);
-var randomSound = Math.floor(Math.random() * 4);
-PlaySound(sounds[randomSound]);
+  var sounds = new Array(grenadeList[1], grenadeList[4], grenadeList[6], grenadeList[7]);
+  var randomSound = Math.floor(Math.random() * 4);
+  PlaySound(sounds[randomSound]);
 }else{
-var sounds = new Array(sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9);
-var randomSound = Math.floor(Math.random() * 9);
-PlaySound(sounds[randomSound]);
+  var randomSound = Math.floor(Math.random() * 9);
+  PlaySound(grenadeList[randomSound]);
 }
-
 };
 
 
@@ -230,7 +179,7 @@ function PlaySound(passedSound){
   console.log(winSound);
   winSound.volume = 0.4;
   winSound.play();
-}
+};
 
 
 
